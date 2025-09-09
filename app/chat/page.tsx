0d44aea46947b130fd1509c20258e169138628c1
goheon <gohoney22@gmail.com>
2025-09-09 20:55:10 +0900

    global.css 제거, MUI 컴포넌트 테스트 추가

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,9 @@
+import Box from '@mui/material/Box'
+
 const MainPage = () => {
   return (
     <div className={''}>
+      <Box className={'text-3xl text-hh-color2 font-bold underline'}>asd</Box>
       <main className={'text-3xl text-hh-color2 font-bold underline'}>
         main
       </main>
