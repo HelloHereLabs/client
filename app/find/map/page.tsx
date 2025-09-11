db1b74d8c0177cc85754160352b1a05c4c0a8b1d
goheon <gohoney22@gmail.com>
2025-09-11 23:39:52 +0900

    feat: 지도 페이지, 기능 추가
    지도 페이지 파일 추가, 카카오 지도 API 부착 훅 추가, 카카오 지도 타입 의존성 추가, 클라이언트 위치 권한 요청 훅 추가

---
+++ app/find/map/page.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { useEffect } from 'react'
+import Box from '@mui/material/Box'
+import { useKakaoMap } from './hooks/useKaKaoMap'
+import { useClientLocation } from './hooks/useClientLocation'
+
+const MapPage = () => {
+  const map = useKakaoMap()
+  const result = useClientLocation(map)
+
+  // 지도 객체가 할당될 때만 지도 중심 이동
+  useEffect(() => {
+    if (map) {
+      console.log('Map instance:', map)
+      var moveLatLon = new window.kakao.maps.LatLng(33.452613, 126.570888)
+      map.setCenter(moveLatLon)
+    }
+  }, [map])
+
+  // 위치 정보가 변경될 때마다 결과 출력
+  useEffect(() => {
+    if (!map) return
+    console.log('Location result:', result)
+    if (result.code === 'OK' && result.data) {
+      console.log(
+        `User location: Latitude ${result.data.x}, Longitude ${result.data.y}`,
+      )
+      //  현위치 마커 추가, 마커 이미지 커스텀
+      const myLocationMarker = new window.kakao.maps.Marker({
+        position: new window.kakao.maps.LatLng(result.data.x, result.data.y),
+        image: new window.kakao.maps.MarkerImage(
+          '/icons/my-location-icon.svg',
+          new window.kakao.maps.Size(24, 28),
+          {
+            offset: new window.kakao.maps.Point(12, 28),
+          },
+        ),
+      })
+      myLocationMarker.setMap(map)
+      //   map.setLevel(2)
+    } else if (result.code === 'denied') {
+      console.log('Location access denied by user.')
+    } else if (result.code === 'not-supported') {
+      console.log('Geolocation is not supported by this browser.')
+    } else if (result.code === 'error') {
+      console.log('An error occurred while retrieving location.')
+    }
+  }, [result])
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Box id="map" sx={{ width: '50vw', height: '30vh' }}></Box>
+    </Box>
+  )
+}
+
+export default MapPage
---
+++ app/find/page.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+
+const FindPage = () => {
+  return (
+    <Container disableGutters className={'find-page'}>
+      <h1>Find Page</h1>
+    </Container>
+  )
+}
+
+export default FindPage
