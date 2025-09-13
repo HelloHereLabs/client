49dec5051059cab9ce367cddef0852004852a062
goheon <gohoney22@gmail.com>
2025-09-13 11:20:39 +0900

    feat: 서비스 인가 레이아웃 추가, 기능 페이지 경로 수정

---
+++ app/(authenticated)/find/components/FindContainer.tsx
@@ -0,0 +1,75 @@
+import Container from '@mui/material/Container'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+
+const FindContainer = () => {
+  return (
+    <Container className="h-full">
+      <Grid container spacing={2} className="h-full">
+        <Grid size={6}>
+          <Paper elevation={3}>
+            <Button
+              variant="contained"
+              className="text-hh-primary bg-hh-secondary w-full"
+            >
+              Search
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid size={6}>
+          <Paper elevation={3}>
+            <Button
+              variant="contained"
+              className="text-hh-primary bg-hh-secondary w-full"
+            >
+              Search
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid size={12}>
+          <Paper elevation={3}>
+            <Button
+              variant="contained"
+              className="text-hh-primary bg-hh-secondary w-full"
+            >
+              Search
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid size={6}>
+          <Paper elevation={3}>
+            <Button
+              variant="contained"
+              className="text-hh-primary bg-hh-secondary w-full"
+            >
+              Search
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid size={6}>
+          <Paper elevation={3}>
+            <Button
+              variant="contained"
+              className="text-hh-primary bg-hh-secondary w-full"
+            >
+              Search
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid size={12}>
+          <Paper elevation={3}>
+            <Button
+              variant="contained"
+              className="text-hh-primary bg-hh-secondary w-full"
+            >
+              Search
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default FindContainer
