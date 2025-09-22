7e8ee5828c935f2b95b634b15a7672c530fcfc73
goheon <gohoney22@gmail.com>
2025-09-23 01:21:56 +0900

    feat: 사용자 위치 전송 소켓 연결 추가

--- lib/config.ts
+++
@@ -1,5 +0,0 @@
-export const BASE_URL =
-  'https://qm81q0oz8a.execute-api.us-west-1.amazonaws.com/local/api/'
-
-export const CHAT_URL =
-  'wss://eqgyhfgc4i.execute-api.us-west-1.amazonaws.com/dev/'
